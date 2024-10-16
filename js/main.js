6cfba4d53787eed0833ea6cb488b38da49b144f9
hanbyeol <dlgksquf8535@gmail.com>
2024-10-16 16:38:40 +0900

    Add intro & result screens
    - 인트로화면 추가 : 유저 닉네임 설정 기능  추가
    - 게임 결과 화면 추가 : 번호 맞추기 성공/실패 각각 다른 텍스트 노출
    - 컨펌 메세지 : 게임 진행 중 리셋버튼  클릭 시 확인창 출력

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
+/* intro */
+let intro = document.querySelector('.intro');
+let startBtn = document.getElementById('startBtn');
+let userName = document.getElementById('userName');
+/* playzone */
+let playZone = document.querySelector('.playZone');
 let userInput = document.getElementById('userInput');
 let playBtn = document.getElementById('playBtn');
 let resetBtn = document.getElementById('resetBtn');
 let chance = document.getElementById('chance');
 let result = document.getElementById('result');
+/* game result */
+let gameEndHide = document.querySelectorAll('.gameEndHide');
+/* Storage */
 let randomNum;
 let chanceNum = 5;
 let history = [];
+let isGameEnd = false;
 
+
+// 이벤트
+startBtn.addEventListener('click', gameStart);
 playBtn.addEventListener('click', gamePlay);
-resetBtn.addEventListener('click', gameReset);
+resetBtn.addEventListener('click', resetBtnAct);
 userInput.addEventListener('focus', function(){
     userInput.value = "";
 });
 // 엔터버튼 이벤트
+userName.addEventListener('keyup', function(e){
+    let key = e.key || e.keyCode;
+    if (key === 'Enter' || key === 13) { gameStart(); }
+});
 userInput.addEventListener('keyup', function(e){
     let key = e.key || e.keyCode;
     if (key === 'Enter' || key === 13) { gamePlay(); }
 });
 
+
+//게임시작
+function gameStart(){
+    let userNameVal = userName.value.trim();
+    if(userNameVal){
+        intro.classList.add('d-none');
+        playZone.classList.remove('d-none');
+        document.querySelector('.userName').textContent = userNameVal;
+    }else{
+        alert("닉네임을 입력하세요!");
+    }
+
+}
 
 //랜덤번호 생성
 function randomNumber(){
@@ -34,12 +64,11 @@ function gamePlay(){
         return;
     }
     if(history.includes(userNum)){  //이미 입력했던 숫자일 경우
-        result.textContent = "이미 입력한 숫자입니다";
+        alert("이미 입력한 숫자입니다");
         return;
     }
     if(userNum == randomNum){
-        result.textContent = `🍀${randomNum}🍀 정답입니다!`;
-        playBtn.disabled = true;
+        gameClear();
         return;
     }else if(userNum > randomNum){
         result.textContent = `Down! (입력숫자 : ${userNum})`;
@@ -54,20 +83,56 @@ function gamePlay(){
     }
 }
 
+function resetBtnAct(){
+    if(isGameEnd){
+        gameReset();
+        isGameEnd = false;
+    }else{
+        if(confirm(`${chanceNum}번의 기회가 남았습니다. 처음부터 다시 시작 하시겠습니까?`)){
+            gameReset();
+        }else{
+            return;
+        }
+    }
+}
+
 //리셋
 function gameReset(){
     randomNumber(); //랜덤번호 생성
+    userInput.disabled = false;
     playBtn.disabled = false; //버튼 활성화
     chanceNum = 5; //남은기회 세팅
     chance.textContent = chanceNum;
     result.textContent = "1~100 사이의 숫자를 입력하세요"
     history = []; //히스토리 비우기
+    userInput.value = "";
+    resetBtn.classList.remove('gameEnd');
+    gameEndHide.forEach(element => {
+        element.classList.remove('d-none');
+    });
+}
+//게임끝
+function gameEnd(){
+    userInput.disabled = true;
+    playBtn.disabled = true;
+    isGameEnd = true;
+    resetBtn.classList.add('gameEnd');
+    gameEndHide.forEach(element => {
+        element.classList.add('d-none');
+    });
+}
+
+
+//게임 클리어
+function gameClear(){
+    result.innerHTML = `${randomNum} 정답입니다! <br><span>💚럭키${userName.value.trim()}💚</span>`;
+    gameEnd();
 }
 
 //게임오버
 function gameOver(){
-    playBtn.disabled = true; //버튼 비활성화
-    result.textContent = `game over 정답 : ${randomNum}`; //게임오버 시 정답 알려주기
+    result.innerHTML = `<span>Game Over</span><br> 정답 : ${randomNum}`; //게임오버 시 정답 알려주기
+    gameEnd();
 }
 
 randomNumber();
